f0b7f9ead1da0439dc6bb06d9b5c8789070c961c
Christian Crumlish <3227378+mediajunkie@users.noreply.github.com>
2022-12-01 08:04:40 -0800

    Revert "Upload new source based on react"
    This reverts commit ed820b3c17214604c906df17c5158755b48ccee3.

--- src/pages/NotFound.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-
-export const NotFoundPage: FC = () => {
-  return (
-    <div className="flex justify-center items-center h-screen bg-transparent">
-      <h3 className="text-yellow text-4xl font-usual-bold">Not Found!</h3>
-    </div>
-  );
-};
--- src/routes/paths.ts
+++
@@ -1,5 +0,0 @@
-export const PATH_ANY = "*";
-export const PATH_ROOT = "/";
-export const PATH_ABOUT = "/about";
-export const PATH_FEEDBACK = "/feedback";
-export const PATH_PRIVACY = "/privacy";
